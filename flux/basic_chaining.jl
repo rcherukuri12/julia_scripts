6dbb1e14ac614d8590797d8940d039df15c06b9c
rcherukuri12 <raja_cherukuri@hotmail.com>
2022-08-04 14:03:38 -0700

    updated

--- flux/basic_chaining.jl
+++ flux/basic_chaining.jl
@@ -1,10 +1,9 @@
 #example of creating chains in flux
-using Flux, BenchmarkTools
-
+using Flux
 model = Chain(x -> 3x^2, x -> 2x, x -> x+1 )
 
 # now take a gradient of this model
-@btime df(x) = Flux.gradient(model,x)[1]
+df(x) = Flux.gradient(model,x)[1]
 
 @show model(5)
 @show df(5)
