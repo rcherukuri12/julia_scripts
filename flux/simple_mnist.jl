cfd4de53c6d1da1f3fa32bddc5efc3546fe23129
rcherukuri12 <raja_cherukuri@hotmail.com>
2022-08-18 15:19:48 -0700

    updated to use elu

--- flux/simple_mnist.jl
+++ flux/simple_mnist.jl
@@ -1,18 +1,21 @@
 #=simple MNIST example
 RESULTS:
-Acccuracy = 0.9546┌
+Acccuracy = 0.9721
 Confusion Matrix:
+┌ Warning: The classes are un-ordered,
+│ using order: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].
+
 10×10 Matrix{Int64}:
- 966     0    7    1    1    6   11    2    4   10
-   0  1115    1    1    1    1    3    6    2    6
-   0     3  973   12    5    0    3   20    4    1
-   1     2    7  960    0   16    2    3   16   13
-   0     0    7    0  942    4    5    4    6   15
-   3     1    1    9    0  836    9    1    6    4
-   6     4    6    1    9    9  922    0    5    1
-   2     2   12    9    3    2    0  975    9   12
-   2     8   16   13    3   12    3    3  918    8
-   0     0    2    4   18    6    0   14    4  939
+ 971     0    6    0    1    4    8    0    3    6
+   0  1121    2    2    0    0    4    2    0    2
+   1     4  999    5    8    0    3    7    2    1
+   2     2    4  984    1   13    1    3   12    9
+   0     0    2    1  955    1    4    2    4   11
+   1     1    0    4    0  860    5    1    6    5
+   0     2    4    1    3    5  929    0    2    0
+   2     1    8    6    3    1    1  999    4    5
+   3     4    7    6    2    6    3    5  938    5
+   0     0    0    1    9    2    0    9    3  965
 
 =#
 using Base64
@@ -29,16 +32,16 @@ x_test, y_test = MNIST(:test)[:];
 x_train = Float32.(x_train) |> gpu
 y_train = Flux.onehotbatch(y_train, 0:9) |> gpu
 model = Chain(
-    Dense(784, 256, relu),
-    Dense(256, 256, relu),
-    Dense(256, 256, relu),
-    Dense(256, 10, relu), softmax
+    Dense(784, 512, elu),
+    Dense(512, 512, elu),
+    Dense(512, 512, elu),
+    Dense(512, 10, elu), softmax
 ) |> gpu
 
 loss(x, y) = Flux.Losses.logitcrossentropy(model(x), y) |> gpu
-optimizer = ADAM(0.00001) |> gpu
+optimizer = ADAM(0.0001) |> gpu
 train_data = [(Flux.flatten(x_train), y_train)] |> gpu
-Flux.@epochs 4000 Flux.train!(loss, Flux.params(model), train_data, optimizer) |> gpu
+Flux.@epochs 8000 Flux.train!(loss, Flux.params(model), train_data, optimizer) |> gpu
 
 # bring back the model from gpu
 model = model |> cpu
